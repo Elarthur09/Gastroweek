ae3dd038a840a0f1d1bf9f7cb16fecbaf1c42e4b
Elarthur09 <arturozarco391@gmail.com>
2022-11-18 00:15:04 -0600

    Ultimas modificaciones

---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'Gastroweek',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
